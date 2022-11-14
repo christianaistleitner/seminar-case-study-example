2f32ecee0f7e1a9e030c50927c3867cdf46bdff0
Christian Aistleitner <christian.aistleitner@outlook.com>
2022-11-14 20:31:25 +0100

    ui

---
+++ ui/src/app/app.component.ts
@@ -0,0 +1,84 @@
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, ViewChild} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  @ViewChild('scrollframe', {static: false})
+  scrollFrame: ElementRef | undefined;
+
+  messages = Array<string>();
+  input: string = "";
+  status: string = "";
+
+  constructor(private changeDetector: ChangeDetectorRef, private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.messages = [];
+    this.getMessages().subscribe({
+      next: data => {
+        console.log(data);
+        this.messages = [data, ...this.messages];
+        this.changeDetector.detectChanges()
+      },
+      error: err => console.error(err)
+    });
+  }
+
+  getMessages(): Observable<string> {
+    return new Observable<string>((observer) => {
+      let eventSource = new EventSource("http://localhost:8080/messages");
+      eventSource.onmessage = (event) => {
+        console.debug('Received event: ', event);
+        observer.next(event.data);
+      };
+      eventSource.onerror = (error) => {
+        // readyState === 0 (closed) means the remote source closed the connection,
+        // so we can safely treat it as a normal situation. Another way
+        // of detecting the end of the stream is to insert a special element
+        // in the stream of events, which the client can identify as the last one.
+        if(eventSource.readyState === 0) {
+          console.log('The stream has been closed by the server.');
+          eventSource.close();
+          observer.complete();
+        } else {
+          observer.error('EventSource error: ' + error);
+        }
+      }
+    });
+  }
+
+  sendMessage(text: string) {
+    console.log("sending message:" + text);
+    this.http
+      .post(
+        "http://localhost:8080/messages",
+        text,
+        {
+          params: {
+            username: "root"
+          }
+        }
+      )
+      .subscribe({
+        next: (data) => {
+          this.input = ""
+          this.status = "Message sent successfully!";
+          this.changeDetector.detectChanges()
+          console.log(data);
+        },
+        error: (error) => console.log(error),
+        complete: () => {
+          console.log('complete');
+        }
+      });
+  }
+
+}
