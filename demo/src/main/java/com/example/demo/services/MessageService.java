a5b040bdadacd03a95fb6c12b4eb7184e9404204
Christian Aistleitner <christian.aistleitner@outlook.com>
2022-11-20 19:15:25 +0100

    Use Sinks.Many instead of the ConnectableFlux hack

--- demo/src/main/java/com/example/demo/services/MessageService.java
+++ demo/src/main/java/com/example/demo/services/MessageService.java
@@ -1,18 +1,12 @@
 package com.example.demo.services;
 
-import com.example.demo.database.MessageRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
+import reactor.core.publisher.Sinks;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.Executors;
-import java.util.function.Consumer;
 
 import static java.time.Duration.ofSeconds;
 
@@ -21,31 +15,20 @@ public class MessageService {
 
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-    private final Scheduler radioScheduler = Schedulers.fromExecutor(Executors.newFixedThreadPool(1));
+    private Sinks.Many<String> messageSink = Sinks.many().multicast().directBestEffort();
 
-    private final MessageRepository messageRepository;
-
-    private Consumer<String> messageSink;
-
-    private final ConnectableFlux<String> messageRadio;
-
-    public MessageService(@Autowired MessageRepository messageRepository) {
-        this.messageRepository = messageRepository;
-        this.messageRadio = Flux.create(fluxSink -> messageSink = fluxSink::next)
-                .sample(ofSeconds(1))
-                .subscribeOn(radioScheduler)
-                .map(it -> "[" + LocalTime.now().format(dtf) + "] " + it)
-                .log()
-                .publish();
+    public MessageService() {
     }
 
     public Flux<String> subscribe() {
-        return messageRadio.autoConnect();
+        return messageSink.asFlux()
+                .sample(ofSeconds(1))
+                .map(it -> "[" + LocalTime.now().format(dtf) + "] " + it);
     }
 
     public Mono<Boolean> send(String text) {
         return Mono.create(monoSink -> {
-            messageSink.accept(text);
+            messageSink.tryEmitNext(text);
             monoSink.success(true);
         });
     }
