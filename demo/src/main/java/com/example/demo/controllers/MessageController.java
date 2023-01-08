92de17548e5a8c027abe23b2e5c21cb8c8da91f9
Christian Aistleitner <christian.aistleitner@outlook.com>
2023-01-08 20:35:09 +0100

    Rename sink

--- demo/src/main/java/com/example/demo/controllers/MessageController.java
+++ demo/src/main/java/com/example/demo/controllers/MessageController.java
@@ -16,18 +16,23 @@ import static java.time.Duration.ofSeconds;
 @CrossOrigin(origins = {"http://localhost:4200"})
 public class MessageController {
 
-    private final Sinks.Many<PacketDto> packetSink = Sinks.many().multicast().directBestEffort();
+    private final Sinks.Many<PacketDto> eventBus = Sinks.many().multicast().directBestEffort();
 
     public MessageController() {
         // TODO: Using Schedulers.enableMetrics(); we could export Micrometer metrics
         // and take a look at thread pool utilization, etc.
     }
 
+    @GetMapping(path = "stream", produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
+    public Flux<PacketDto> subscribe(@RequestParam String username) {
+        return eventBus.asFlux().filter(it -> it.getRecipient().equals(username));
+    }
+
     @PostMapping(path = "send", produces = {MediaType.TEXT_PLAIN_VALUE})
     public String send(@RequestBody String text, @RequestParam String username, @RequestParam String recipient) {
         MessageDto message = new MessageDto(text, username, recipient);
-        packetSink.asFlux()
-                .doOnSubscribe(ignore -> packetSink.tryEmitNext(message))
+        eventBus.asFlux()
+                .doOnSubscribe(ignore -> eventBus.tryEmitNext(message))
                 .filter(it -> it.getType() == PacketDto.Type.ACK && it.getRecipient().equals(username))
                 .cast(AckDto.class)
                 .any(it -> it.getMessageId().equals(message.getId()))
@@ -40,11 +45,6 @@ public class MessageController {
 
     @PostMapping(path = "ack", produces = {MediaType.TEXT_PLAIN_VALUE})
     public void ack(@RequestBody String messageId, @RequestParam String username, @RequestParam String recipient) {
-        packetSink.tryEmitNext(new AckDto(username, messageId, recipient));
-    }
-
-    @GetMapping(path = "stream", produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
-    public Flux<PacketDto> subscribe(@RequestParam String username) {
-        return packetSink.asFlux().filter(it -> it.getRecipient().equals(username));
+        eventBus.tryEmitNext(new AckDto(username, messageId, recipient));
     }
 }
